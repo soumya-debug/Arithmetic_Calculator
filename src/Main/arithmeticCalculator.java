19f7a1e04769ec21e10f2c2b7150a6c08026e91c
Soumya Ranjan Rout <soumya261412@gmail.com>
2023-05-06 22:16:03 +0530

    First-Commit

---
+++ src/Main/arithmeticCalculator.java
@@ -0,0 +1,59 @@
+package Main;
+import java.util.Scanner;
+
+public class arithmeticCalculator {
+
+	public static int add(int x, int y)
+	{
+		int result = x + y;
+		return result;
+	}
+	public static int sub(int x, int y)
+	{
+		int result = x - y;
+		return result;
+	}
+	public static int mult(int x, int y)
+	{
+		int result = x * y;
+		return result;
+	}
+	public static int div(int x, int y)
+	{
+		int result = x / y;
+		return result;
+	}
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int n1,n2;
+		System.out.println("Enter first number:");
+		n1 = sc.nextInt();
+		System.out.println("Enter second number:");
+		n2 = sc.nextInt();
+		int result;
+		System.out.println("Enter your selection: 1 Addition(+), 2 Substraction(-), 3 Multiplication(*), 4 Division(/):");
+		int choose;
+		choose = sc.nextInt();
+		switch (choose){
+		case 1:
+			System.out.println(add( n1,n2));
+			break;
+		case 2:
+			System.out.println(sub( n1,n2));
+			break;      
+		case 3:
+			System.out.println(mult( n1,n2));
+			break;
+		case 4:
+			System.out.println(div( n1,n2));
+			break;
+		default:
+			System.out.println("Oops! Wrong choice, Please choose between 1 2 3 4");
+
+		}
+
+	}
+
+}
+
